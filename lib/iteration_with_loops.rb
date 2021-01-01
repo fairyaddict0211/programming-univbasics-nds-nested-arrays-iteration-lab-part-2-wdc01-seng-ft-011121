cdd096bb9ca21a76cd69a60c992f2a9df4fabf0f
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-01 20:31:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,11 @@
 def find_min_in_nested_arrays(src)
   outer_results = []
 row_index = 0
-while row_index < spice_rack.count do
+while row_index < src.count do
   element_index = 0
   longest_string_element = ""
-  while element_index < spice_rack[row_index].count do
+  while element_index < src[row_index].count do
+    
+    
  
 end
