3f0531ef2c7fa35e6cade12e34a5a4f2b014c0bf
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-01 20:35:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,25 @@
-def find_min_in_nested_arrays(array)
-  outer_results = []
-row_index = 0
-while row_index < src.count do
-  element_index = 0
-  longest_string_element = ""
-  while element_index < src[row_index].count do
-     if src[row_index][element_index].length > longest_string_element.length
-      longest_string_element = src[row_index][element_index]
+def find_min_in_nested_arrays(src)
+  row_index = 0 
+  new_array = [] 
+
+  while row_index < src.count do
+
+    element_index = 0 
+    lowest_temperatures = -1
+    while element_index < src[row_index].count do 
+
+        if src[row_index][element_index] == src[row_index].min 
+
+         lowest_temperatures = src[row_index][element_index]
+
+        end
+      element_index +=1  
+
     end
-    element_index += 1
-  end
-    
- 
-end
+    row_index +=1 
+
+    new_array << lowest_temperatures
+
+  end 
+   new_array
+end  
